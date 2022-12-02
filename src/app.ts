3c59a456963ac3fc8014f071a0b293d6b2efbc23
wladszGL <vlad098ff@gmail.com>
2022-12-02 05:56:20 +0200

    Task 4.01 started

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-redeclare */
+
 showHello('greeting', 'TypeScript');
 
 function showHello(divName: string, name: string) {
@@ -9,12 +11,22 @@ function showHello(divName: string, name: string) {
 // Task 02.01
 enum Category {JavaScript, CSS, HTML, TypeScript, Angular}
 
-type Book = {
+// type Book = {
+//     id: number;
+//     title: string;
+//     author: string;
+//     available: boolean;
+//     category: Category;
+// };
+
+interface Book {
     id: number;
     title: string;
     author: string;
     available: boolean;
     category: Category;
+
+    pages?: number;
 };
 
 function getAllBooks(): readonly Book[] {
@@ -31,7 +43,7 @@ function getAllBooks(): readonly Book[] {
     return books;
 }
 
-function logFirstAvailable(books: readonly Book[]): void {
+function logFirstAvailable(books: readonly Book[] = getAllBooks()): void {
     console.log(`Number of books: ${books.length}`);
 
     const title = books.find(({available}) => available)?.title;
@@ -42,7 +54,7 @@ logFirstAvailable(getAllBooks());
 
 
 
-function getBookTitlesByCategory(inputCategory: Category): string[] {
+function getBookTitlesByCategory(inputCategory: Category = Category.JavaScript): string[] {
     const books = getAllBooks();
 
     return books
@@ -115,3 +127,96 @@ function createCustomer(name: string, age?: number, city?: string): void {
 createCustomer('Ann');
 createCustomer('Ann', 20);
 createCustomer('Ann', 20, 'Kyiv');
+
+logBookTitles(getBookTitlesByCategory());
+
+logFirstAvailable();
+
+// function getBookByID(id: number): Book {
+function getBookByID(id: Book['id']): Book | undefined {
+    const books = getAllBooks();
+    return books.find(book => book.id === id);
+}
+
+console.log(getBookByID(1));
+
+function checkoutBooks(customer: string, ...bookIDs: number[]): string[] {
+    console.log(`Customer name: ${customer}`);
+
+    return bookIDs
+        .map(id => getBookByID(id)) // не дуже оптимальний варіант, але для малих масивів ОК
+        .filter(book => book.available)
+        .map(book => book.title);
+}
+
+checkoutBooks('NoName Customer', 1, 3, 4);
+
+const myBooks = checkoutBooks('Ann', 1, 2, 4 );
+console.log(myBooks);
+
+
+// Task 03.03 Function Overloading
+function getTitles(author: string): string[];
+function getTitles(available: boolean): string[];
+function getTitles(id: number, available: boolean): string[];
+// function getTitles(...args: unknown[]): string[] {
+function getTitles(...bookProperties: [string|boolean] | [number, boolean]): string[] {
+    const books = getAllBooks();
+    let foundTitles: string[];
+    if (bookProperties.length === 1) {
+        const [property] = bookProperties;
+
+        if (typeof property === 'string') {
+            return books.filter(book => book.author === property).map(book => book.title);
+        } else if (typeof property === 'boolean') {
+            return books.filter(book => book.available === property).map(book => book.title);
+        }
+    } else if (bookProperties.length === 2) {
+        const [id, available] = bookProperties;
+
+        if (typeof id === 'number' && typeof available === 'boolean') { // optional, not neccesary
+            return books.filter(book => book.id === id && book.available === available).map(book => book.title);
+        }
+    }
+}
+
+console.log(getTitles(1, true));
+console.log(getTitles('Lea Verou'));
+
+
+// Task 03.04 Assertion Functions
+function assertStringValue(data: any): asserts data is string {
+    if (typeof data !== 'string') {
+        throw new Error('value should have been a string');
+    }
+}
+
+
+function bookTitleTransform(title: any): string {
+    assertStringValue(title);
+    return [...title].reverse().join('');
+}
+
+console.log(bookTitleTransform('Learn TypeScript'));
+console.log(bookTitleTransform(123));
+
+
+
+// ----------------------------------------------------------------------
+
+// Task 04.01 Defining an Interface
+function printBook(book: Book): void {
+    console.log(`${book.title} by ${book.author}`);
+}
+
+const myBook: Book = {
+    id: 5,
+    title: 'Colors, Backgrounds, and Gradients',
+    author: 'Eric A. Meyer',
+    available: true,
+    category: Category.CSS,
+    // year: 2015,
+    // copies: 3
+};
+
+printBook(myBook);
